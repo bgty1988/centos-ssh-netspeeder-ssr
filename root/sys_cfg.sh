692fadb1a9b737f6f74eeffd3c7c674c074f1f4b
shijh666 <qq329889612@163.com>
2017-07-01 11:44:10 +0800

    test

--- root/sys_cfg.sh
+++ root/sys_cfg.sh
@@ -11,24 +11,24 @@ echo "root:${ROOT_PASSWORD:-$DEFAULT_PASSWORD}" | chpasswd
 # -----------------------------------------------------------------------------
 # Configure ShadowsocksR
 # -----------------------------------------------------------------------------
-sed -i \
-	-e 's/"server_port".*/"server_port": '$SSR_PORT',/' \
-	-e 's/"password".*/"password": "'${SSR_PASSWORD:-$DEFAULT_PASSWORD}'",/' \
-	-e 's/"method".*/"method": "'${SSR_METHOD:-rc4-md5}'",/' \
-	-e 's/"protocol".*/"protocol": "'${SSR_PROTOCOL:-auth_sha1_v4}'",/' \
-	-e 's/"obfs".*/"obfs": "'${SSR_OBFS:-tls1.2_ticket_auth}'",/' \
-	/root/shadowsocksr/shadowsocks/user-config.json
+# sed -i \
+	# -e 's/"server_port".*/"server_port": '$SSR_PORT',/' \
+	# -e 's/"password".*/"password": "'${SSR_PASSWORD:-$DEFAULT_PASSWORD}'",/' \
+	# -e 's/"method".*/"method": "'${SSR_METHOD:-rc4-md5}'",/' \
+	# -e 's/"protocol".*/"protocol": "'${SSR_PROTOCOL:-auth_sha1_v4}'",/' \
+	# -e 's/"obfs".*/"obfs": "'${SSR_OBFS:-tls1.2_ticket_auth}'",/' \
+	# /root/shadowsocksr/shadowsocks/user-config.json
 	
 # -----------------------------------------------------------------------------
 # Configure supervisor
 # -----------------------------------------------------------------------------
-sed -i \
-	-e 's/port=.*/port='$SVD_PORT'/' \
-	-e 's/username=.*/username='${SVD_USERNAME:-$DEFAULT_USERNAME}'/' \
-	-e 's/password=.*/password='${SVD_PASSWORD:-$DEFAULT_PASSWORD}'/' \
-	/etc/supervisord.conf
+# sed -i \
+	# -e 's/port=.*/port='$SVD_PORT'/' \
+	# -e 's/username=.*/username='${SVD_USERNAME:-$DEFAULT_USERNAME}'/' \
+	# -e 's/password=.*/password='${SVD_PASSWORD:-$DEFAULT_PASSWORD}'/' \
+	# /etc/supervisord.conf
 
-sed -i \
-	-e 's/autostart=.*/autostart='${NET_SPEEDER_AUTOSTART:-true}'/' \
-	/etc/supervisord.d/net_speeder.conf
+# sed -i \
+	# -e 's/autostart=.*/autostart='${NET_SPEEDER_AUTOSTART:-true}'/' \
+	# /etc/supervisord.d/net_speeder.conf
 	
